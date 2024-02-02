0d10c2f44cd6e5807a911d291a633444f4d5245b
adityaparikh7 <asparikh03@gmail.com>
2024-02-02 16:48:00 +0530

    first commit

---
+++ exp 3/mathcaptcha.py
@@ -0,0 +1,27 @@
+import random
+
+
+def generate_math():
+    operators = ['+', '-', '*', '/']
+    num1 = random.randint(1, 10)
+    num2 = random.randint(1, 10)
+    operator = random.choice(operators)
+    question = f"{num1} {operator} {num2}"
+    result = eval(question)
+    return {"question": question, "answer": result}
+
+def validate_captcha(answer, result_actual):
+    return answer == result_actual
+
+
+def main():
+    math = generate_math()
+    print(f"Question: {math['question']}")
+    answer = int(input("Enter the answer: "))
+    if validate_captcha(answer, math['answer']):
+        print("You are human!")
+    else:
+        print("You are a robot!")
+
+if __name__ == "__main__":
+    main()
