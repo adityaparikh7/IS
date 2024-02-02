9e1276b74ffcd79a1b402b5c3ad83c7ecd51d7dd
adityaparikh7 <asparikh03@gmail.com>
2024-02-02 15:01:43 +0530

    added user input for length

---
+++ exp 3/textcaptcha.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+def generate_captcha(length):
+    characters = string.ascii_letters + string.digits
+    captcha = ''.join(random.choice(characters) for _ in range(length))
+    return captcha
+
+def main():
+    length = int(input("Enter the length of the CAPTCHA: "))
+    print(generate_captcha(length))
+
+
+if __name__ == "__main__":
+    main()
