c48f872a6b058f28ad4c127b36c371d1835249da
Aditya Parikh <asparikh03@gmail.com>
2024-02-09 17:45:27 +0530

    added special characters

--- exp 3/textcaptcha.py
+++ exp 3/textcaptcha.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def generate_captcha(length):
-    characters = string.ascii_letters + string.digits
+    characters = string.ascii_letters + string.digits + string.punctuation
     captcha = ''.join(random.choice(characters) for _ in range(length))
     return captcha
 
